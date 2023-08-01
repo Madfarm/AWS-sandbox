9c60024b09a4528ee0b45a2ca1e127bde2ae94fd
AnthonyScott <anthony.scott1005@gmail.com>
2023-08-01 17:05:19 -0400

    Add configuration for postgres connection

--- gopherAPI/Helpers/GlobalUsings.cs
+++ gopherAPI/Helpers/GlobalUsings.cs
@@ -2,3 +2,4 @@ global using GopherAPI.Entities;
 global using Microsoft.Extensions.Options;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
+global using GopherAPI.Data;
