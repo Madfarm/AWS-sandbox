568fdb999a2a11d7cb316b10ffa5dcb85d36fa16
AnthonyScott <anthony.scott1005@gmail.com>
2023-08-01 17:01:22 -0400

    Add gopher model

--- gopherAPI/Data/GopherContext.cs
+++ gopherAPI/Data/GopherContext.cs
@@ -4,13 +4,12 @@ public class GopherContext :  DbContext
 {
     protected readonly IConfiguration Configuration;
 
-    public GopherContext(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+    public GopherContext(IConfiguration configuration) => Configuration = configuration;
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
         options.UseNpgsql(Configuration.GetConnectionString("GopherContextPostgres"));
     }
+
+    public DbSet<Gopher> Gophers { get; set; }
 }
--- gopherAPI/Entities/Gopher.cs
+++ gopherAPI/Entities/Gopher.cs
@@ -1,1 +1,9 @@
 namespace GopherAPI.Entities;
+
+
+public class Gopher 
+{
+    public int Id { get; set; }
+    public int Age { get; set;}
+    public string? Name { get; set; }
+}
