a7b6b9e0461866ab73af1676bfa5f7ef942ea2e7
AnthonyScott <anthony.scott1005@gmail.com>
2023-08-03 03:44:23 -0400

    Clean up DbContext

--- gopherAPI/Data/GopherContext.cs
+++ gopherAPI/Data/GopherContext.cs
@@ -2,10 +2,6 @@ namespace GopherAPI.Data;
 
 public class GopherContext :  DbContext
 {
-    protected readonly IConfiguration Configuration;
-
     public GopherContext(DbContextOptions<GopherContext> options) : base(options) { }
     public DbSet<Gopher> Gophers { get; set; }
-
-    
 }
